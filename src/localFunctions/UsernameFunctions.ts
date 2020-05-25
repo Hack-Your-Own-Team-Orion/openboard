f9d97fb6a1d4d246eb628fb7cb69a72bb2a21983
Nayil <n.arana1@outlook.com>
2020-05-25 14:48:02 -0500

    Add username function colorFromUsername

--- src/localFunctions/UsernameFunctions.ts
+++ src/localFunctions/UsernameFunctions.ts
@@ -9,7 +9,18 @@ export function usernameFromIp(ip: string): string {
 }
 
 export function colorFromUsername(username: string): string {
-    return "";
+    var colors = ["#1abc9c", "#2ecc71", "#3498db", "#9b59b6", "#34495e", "#16a085", "#27ae60"
+	, "#2980b9", "#8e44ad", "#2c3e50", "#f1c40f", "#e67e22", "#e74c3c", "#ecf0f1", "#95a5a6"
+	, "#f39c12", "#d35400", "#c0392b", "#bdc3c7", "#7f8c8d"];
+    var hash = 0;
+	if (username.length === 0) return hash;
+    for (var i = 0; i < username.length; i++) {
+        hash = username.charCodeAt(i) + ((hash << 5) - hash);
+        hash = hash & hash;
+    }
+    hash = ((hash % colors.length) + colors.length) % colors.length;
+	console.log(colors[hash])
+    return colors[hash];
 }
 
 export default {
